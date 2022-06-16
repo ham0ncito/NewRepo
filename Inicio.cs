3f3f82031ef2cad0cb498a357671b6935eb9dace
ham0ncito <106882518+ham0ncito@users.noreply.github.com>
2022-06-15 21:05:41 -0600

    c
    c

---
+++ Inicio.cs
@@ -0,0 +1,15 @@
+namespace GerizimZZ
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
