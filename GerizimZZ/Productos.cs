f89ff5802e26713ff8e475742e7f5fe5b57ccb50
memo01996 <36063403+memo01996@users.noreply.github.com>
2022-06-15 22:16:40 -0600

    Add Encapsulate fields

--- GerizimZZ/Productos.cs
+++ GerizimZZ/Productos.cs
@@ -10,12 +10,12 @@ namespace GerizimZZ
     {
         /* atributos esenciales de los productos */
 
-        public string id_producto;
-        public double precio_producto;
-        public string nombre;
-        public string codigoBarra;
-        public string descripcion;
-        public int estado;
+        private string id_producto;
+        private double precio_producto;
+        private string nombre;
+        private string codigoBarra;
+        private string descripcion;
+        private int estado;
 
         string fuente = "DESKTOP-2H6N4DP";
         string bd = "Gerizim";
@@ -29,7 +29,14 @@ namespace GerizimZZ
             + "; Integrated Security=" + seguridad
             ;
       
-        Public SqlConnection conection = new SqlConnection();
+        public string Id_producto { get => id_producto; set => id_producto = value; }
+        public double Precio_producto { get => precio_producto; set => precio_producto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string CodigoBarra { get => codigoBarra; set => codigoBarra = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Estado { get => estado; set => estado = value; }
+
+        conection = new SqlConnection();
           
 
         // metodos
