e3d0bad7e9a5bad91e0b483d2d8d839ef1078bc0
CesarOmarRueda24 <107312905+CesarOmarRueda24@users.noreply.github.com>
2022-06-23 19:44:54 -0600

    Barra y opciones 2

--- GerizimZZ/Program.cs
+++ GerizimZZ/Program.cs
@@ -11,7 +11,7 @@ namespace GerizimZZ
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Barra_preview());
+            Application.Run(new FrmLogin());
         }
     }
 }
