059874fae8382181da8532ffde3199d260014903
memo01996 <36063403+memo01996@users.noreply.github.com>
2022-06-25 08:31:59 -0600

    i dont know

--- GerizimZZ/Cl_Login.cs
+++ GerizimZZ/Cl_Login.cs
@@ -5,7 +5,7 @@ namespace GerizimZZ
 {
     internal class Cl_Login
     {
-        SqlConnection con = new SqlConnection("Data Source=TATO180\\SQLEXPRESS;Initial Catalog=Gerizim; Integrated Security=True;");
+        SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=Gerizim; Integrated Security=True;");
         private String nombreUsuario { get; set; }
         private string contraseñaUsuario { get; set; }
 
