a39ed17677ec02e51df9455feba8db3d8b91fce5
ham0ncito <lil_ham0n@hotmail.com>
2022-07-01 16:29:34 -0600

    avisos

---
+++ GerizimZZ/CsMensajes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GerizimZZ
+{
+   
+    internal class CsMensajes
+    {
+        private string nombre_usuario;
+        private string fecha = "Hoy";
+        private string idMensaje;
+        private string mensajeenviado = "Hola";
+        public string Mensajeenviado { get => mensajeenviado; set => mensajeenviado = value; }
+        public string Fecha { get => fecha; set => fecha = value; }
+        public string Nombre_usuario { get => nombre_usuario; set => nombre_usuario = value; }
+
+        SqlConnection conexion = new SqlConnection("Data Source =localhost ; Initial Catalog =Gerizim ; Integrated Security = True");
+
+    }
+}
--- GerizimZZ/Mensaje.cs
+++ GerizimZZ/Mensaje.cs
@@ -13,16 +13,7 @@ namespace GerizimZZ
 {
     public partial class Mensaje : UserControl
     {
-        private string nombre_usuario;
-        private string fecha = "Hoy";
-        private string idMensaje; 
-        private string mensajeenviado = "Hola";
-        public string Mensajeenviado { get => mensajeenviado; set => mensajeenviado = value; }
-        public string Fecha { get => fecha; set => fecha = value; }
-        public string Nombre_usuario { get => nombre_usuario; set => nombre_usuario = value; }
-
-        SqlConnection conexion = new SqlConnection("Data Source =localhost ; Initial Catalog =Gerizim ; Integrated Security = True");
-
+        
         public Mensaje()
         {
             InitializeComponent();
