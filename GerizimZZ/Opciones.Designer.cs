e74531200960317adc2acbc9cc9a71f8acd6c01b
ham0ncito <lil_ham0n@hotmail.com>
2022-07-01 14:47:10 -0600

    actualizaciones

--- GerizimZZ/Opciones.Designer.cs
+++ GerizimZZ/Opciones.Designer.cs
@@ -77,6 +77,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Opciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Opciones";
             this.ResumeLayout(false);
             this.PerformLayout();
