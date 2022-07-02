6537f6eabdbafb51123927418bed1b52d3cab53b
ham0ncito <lil_ham0n@hotmail.com>
2022-07-01 18:25:21 -0600

    actualizacion

---
+++ GerizimZZ/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerizimZZ
+{
+    internal class Usuario
+    {
+        //CI_Login cs = new CI_Login; 
+    }
+}
