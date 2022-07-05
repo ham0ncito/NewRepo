90da3713876a50c9bd158d02ec87994a1f8de6c2
memo01996 <36063403+memo01996@users.noreply.github.com>
2022-07-04 23:41:48 -0600

    lblproducto a inventario

--- GerizimZZ/Program.cs
+++ GerizimZZ/Program.cs
@@ -11,7 +11,7 @@ namespace GerizimZZ
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmInventario());
+            Application.Run(new FrmLogin());
             
             
         }
