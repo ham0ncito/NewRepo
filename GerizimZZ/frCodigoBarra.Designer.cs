57b2b65f9c3d92f503aaf66b86bc14b4685f4cf2
ham0ncito <lil_ham0n@hotmail.com>
2022-07-08 10:19:44 -0600

    actua

--- GerizimZZ/frCodigoBarra.Designer.cs
+++ GerizimZZ/frCodigoBarra.Designer.cs
@@ -69,7 +69,6 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(225, 56);
             this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(134, 25);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
